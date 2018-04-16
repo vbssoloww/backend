2bacad0df47f70192cbd0ebd9490a2230845aff0
Kris Kalavantavanich <kkalavantavanich@gmail.com>
2018-04-17 03:37:29 +0700

    Basic routing

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const api = require('./routes/api');
 
 const app = express();
 
 const PORT = 3145;
 
+
+app.use(helmet());
+app.use(morgan('dev'))
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
 app.get('/', (req, res) => {
     res.send("Hello!");
 })
+
+app.use('/api', api);
 
 
 app.listen(PORT, () => {
