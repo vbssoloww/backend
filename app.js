15971f8cf5f4e23ae98a3e4063c44dc947ac3215
Kris Kalavantavanich <kkalavantavanich@gmail.com>
2018-04-17 06:41:13 +0700

    Added urlencoded bodyparser

--- app.js
+++ app.js
@@ -4,24 +4,26 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+// Routes
 const api = require('./routes/api');
 
+// Application
 const app = express();
 
+// Config
 const PORT = 3145;
 
 
 app.use(helmet());
 app.use(morgan('dev'))
 app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send("Hello!");
 })
 
 app.use('/api', api);
-
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
