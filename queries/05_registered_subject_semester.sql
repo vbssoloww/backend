d01e4a0bb5d0fcc6ced667c00ac486ecdbb982ce
Kris Kalavantavanich <kkalavantavanich@gmail.com>
2018-04-21 19:18:50 +0700

    Added year, semester and grade to registered

--- queries/05_registered_subject_semester.sql
+++ queries/05_registered_subject_semester.sql
@@ -1,4 +1,4 @@
-SELECT stdy.SubjectID, s.Credit
+SELECT stdy.SubjectID, s.NameEN, s.Credit, stdy.Year, stdy.Semester, stdy.Grade
 FROM `subject` s, studies stdy
 WHERE stdy.StudentID = ? AND
       stdy.Year = ? AND
